5ffc639ea0a4295f9aa1b37fb1ce8cf2531f8283
DESKTOP-20BACJI\Admin <binh.ngv.dev@gmail.com>
2023-08-19 10:19:17 +0700

    install unit test lib

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 const PORT_FOR_PROXY = process.env.ASPNETCORE_HTTPS_PORT;
 export default defineConfig({
-    plugins: [react(), basicSsl()],
-    server: {
-        https: true,
-        proxy: {
-            "/api": {
-                target: `https://localhost:${PORT_FOR_PROXY}`,
-                secure: false,
-            },
-        },
+  plugins: [react(), basicSsl()],
+  server: {
+    https: true,
+    proxy: {
+      "/api": {
+        target: `https://localhost:${PORT_FOR_PROXY}`,
+        secure: false,
+      },
     },
-    resolve: {
-        extensions: [".vue", ".svg"],
-        alias: [{ find: "@assets", replacement: "/src/assets" }],
-    },
+  },
+  resolve: {
+    extensions: [".vue", ".svg"],
+    alias: [
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@view", replacement: "/src/view" },
+    ],
+  },
 });
